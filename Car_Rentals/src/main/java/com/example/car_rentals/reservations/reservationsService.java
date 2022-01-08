6303eb4a12da019e618ec944c854ab9532bd464a
zengab <94101800+zengab@users.noreply.github.com>
2022-01-08 18:16:04 +0200

    schema updates

--- Car_Rentals/src/main/java/com/example/car_rentals/reservations/reservationsService.java
+++ Car_Rentals/src/main/java/com/example/car_rentals/reservations/reservationsService.java
@@ -1,12 +1,11 @@
 package com.example.car_rentals.reservations;
-import java.util.Dictionary;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.Tuple;
 
 @Service
 
@@ -15,9 +14,13 @@ import javax.persistence.Tuple;
 public class reservationsService {
     private final reservationsRepository reservationsRepository;
 
+
     @Autowired
-    public reservationsService(reservationsRepository reservationsRepository) {
+
+    public reservationsService(reservationsRepository reservationsRepository ) {
         this.reservationsRepository = reservationsRepository;
+
+
     }
 
     public List<reservations> getReservations() {
@@ -25,10 +28,43 @@ public class reservationsService {
     }
 
     public void addNewReservation(reservations reservations) {
-        this.reservationsRepository.insert(reservations.getLicense(), reservations.getUsername(),reservations.getStart_date(),reservations.getEnd_date(),reservations.getReservation_status(),reservations.getPayment(),reservations.getPayment_status());
-;    }
+        this.reservationsRepository.insert(reservations.getLicense(), reservations.getUsername(), reservations.getStart_date(), reservations.getEnd_date(), reservations.getReservation_status(), reservations.getPayment(), reservations.getPayment_status());
+        ;
+    }
 
-    public  List<Map<String,Object>> getAllDetails() {
+    public List<Map<String, Object>> getAllDetails() {
         return this.reservationsRepository.getAllDetails();
     }
+
+    public List<reservations> reservation_filter(reservations reservations) {
+        List<String> usernames = new ArrayList<>();
+        List<String> licenses = new ArrayList<>();
+        List<LocalDate> dates = new ArrayList<>();
+        List<Integer> payments = new ArrayList<>();
+        if (reservations.getUsername().equals("null")) {
+            usernames = this.reservationsRepository.getAllUsers();
+        } else {
+            usernames.add(reservations.username);
+        }
+        if (reservations.getLicense().equals("null")) {
+            licenses = this.reservationsRepository.getAllLicenses();
+        } else {
+            licenses.add(reservations.License);
+        }
+        if (reservations.getStart_date().equals("null")) {
+            dates = this.reservationsRepository.getAllDates();
+        } else {
+            dates.add(reservations.start_date);
+        }
+        if (reservations.getPayment() == 0) {
+            payments = this.reservationsRepository.getAllPayments();
+        } else {
+            payments.add(reservations.payment);
+        }
+    return this.reservationsRepository.reservations_filter(usernames,licenses,dates,payments) ;
+    }
 }
+
+
+
+
